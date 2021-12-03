b53e2a230a07a80fa4d8d55d36fff759147318b1
zwhjr <82550160+zwhjr@users.noreply.github.com>
2021-12-02 18:29:07 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,13 @@ var cat, catImg1,catImg2;
 var mouse, mouseImg1,mouseImg2;
 
 function preload() {
-    bg = loadImage("images/garden.png");
-    catImg1= loadAnimation("images/catSitting.png");
-    catImg2=loadAnimation("images/catWalking1.png","images/catWalking2.png");
-    catImg3= loadAnimation("images/catHappy.png");
-    mouseImg1=loadAnimation("images/mouseGift.png");
-    mouseImg2= loadAnimation("images/mouseTease1.png","images/mouseTease2.png");
-    mouseImg3=loadAnimation("images/mouseHappy.png");
+    bg = loadImage("garden.png");
+    catImg1= loadAnimation("catSitting.png");
+    catImg2=loadAnimation("catWalking1.png","catWalking2.png");
+    catImg3= loadAnimation("catHappy.png");
+    mouseImg1=loadAnimation("mouseGift.png");
+    mouseImg2= loadAnimation("mouseTease1.png","mouseTease2.png");
+    mouseImg3=loadAnimation("mouseHappy.png");
 }
 
 function setup(){
@@ -34,30 +34,6 @@ function draw() {
 
 //CHOOSE THE CORRECT IF BLOCK FOR CHECKING COLLISION 
 
-    /*if(cat.y - mouse.x < (cat.height - mouse.width)/2)
-    { 
-        cat.velocityX=0;
-        cat.addAnimation("catLastImage", catImg3);
-        cat.x =300;
-        cat.scale=0.2;
-        cat.changeAnimation("catLastImage");
-        mouse.addAnimation("mouseLastImage", mouseImg3);
-        mouse.scale=0.15;
-        mouse.changeAnimation("mouseLastImage");
-    }*/
-
-    /*if(cat.x - mouse.y < (cat.width - mouse.y)/2)
-    { 
-        cat.velocityX= -4;
-        cat.addAnimation("catLastImage", catImg3);
-        cat.x =300;
-        cat.scale=0.2;
-        cat.changeAnimation("catLastImage");
-        mouse.addAnimation("mouseLastImage", mouseImg3);
-        mouse.scale=0.15;
-        mouse.changeAnimation("mouseLastImage");
-    }*/
-
     if(cat.x - mouse.x < (cat.width - mouse.width)/2)
     { 
         cat.velocityX=0;
@@ -69,18 +45,6 @@ function draw() {
         mouse.scale=0.15;
         mouse.changeAnimation("mouseLastImage");
     }
-
-    /*if(cat.y - mouse.y < (cat.height - mouse.height)/2)
-    { 
-        cat.velocityX= 4;
-        cat.addAnimation("catLastImage", catImg3);
-        cat.x =300;
-        cat.scale=0.2;
-        cat.changeAnimation("catLastImage");
-        mouse.addAnimation("mouseLastImage", mouseImg3);
-        mouse.scale=0.15;
-        mouse.changeAnimation("mouseLastImage");
-    }*/
 
     drawSprites();
 }
